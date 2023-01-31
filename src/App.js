2f1dc5a51cec50fac9d4932871f3c520307104a0
Jack Watters <johnwatt@usc.edu>
2023-01-31 10:16:42 -0800

    app logic for score and displaying components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import Game from './components/Game/Game';
 import Header from './components/Header/Header';
 import './styles/App.css';
 
 const App = () => {
-  // TODO
-  const score = 0;
-  const highScore = 0;
+  const [level, setLevel] = useState(1);
+  const handleNewLevel = () => setLevel(level + 1);
+  const resetLevel = () => setLevel(1);
+
+  const [score, setScore] = useState(0);
+  const resetScore = () => setScore(0);
+  useEffect(() => setScore(level - 1), [level, score]);
+
+  const resetGame = () => {
+    resetLevel();
+    resetScore();
+  };
+
+  const [highScore, setHighScore] = useState(0);
+  useEffect(() => {
+    if (score > highScore) setHighScore(score);
+  }, [score, highScore]);
 
   return (
-    <container className="container">
+    <div className="container">
       <Header score={score} highScore={highScore} />
-      <Game />
-    </container>
+      <Game
+        level={level}
+        incrementLevel={handleNewLevel}
+        resetGame={resetGame}
+      />
+    </div>
   );
 };
 
